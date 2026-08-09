a5bf918b649b49c236fc3ec2b76f1cda3c22ce46
Leandro74377 <leandrobaque4@gmail.com>
2026-08-09 00:18:46 -0500

    Add files via upload

---
+++ frontend/src/app/router/index.tsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../../App";
+import LandingPage from "../../pages/LandingPage";
+import Login from "../../pages/Login";
+import Register from "../../pages/Register";
+import NewAnalysisPage from "../../pages/NewAnalysisPage";
+import DashboardPage from "../../pages/DashboardPage";
+import NotFoundPage from "../../pages/NotFoundPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "demo",
+        element: <LandingPage initialSection="demo" />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "analisis/nuevo",
+        element: <NewAnalysisPage />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardPage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+]);
